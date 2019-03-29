740624cd3bbb169cbd5864c61b07e3e39edb88d4
Hamza Zaman <hamza.zaman@outlook.com>
2019-03-29 20:26:08 +0000

    Task 2 done: create vehicles table in the laracarent database

---
+++ unit4/laracarent/database/migrations/2019_03_29_202039_create_vehicles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->enum('category', ['car', 'truck'])->default('car');
+            $table->string('brand', 30)->nullable();
+            $table->decimal('dailyrate', 6, 2)->default(9.99);
+            $table->string('description', 256)->nullable();
+            $table->string('image', 256)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
