d2e6c98bb4d2af58458f4a4a215446d0592a9d7e
Hamza Zaman <hamza.zaman@outlook.com>
2019-03-29 21:00:49 +0000

    Task 7 Create function

--- unit4/laracarent/app/Http/Controllers/VehicleController.php
+++ unit4/laracarent/app/Http/Controllers/VehicleController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Vehicle;
+
 use Illuminate\Http\Request;
 
 class VehicleController extends Controller
@@ -23,7 +25,7 @@ class VehicleController extends Controller
      */
     public function create()
     {
-        //
+        return view('vehicles.create');
     }
 
     /**
@@ -34,7 +36,48 @@ class VehicleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      // form validation
+      $vehicle = $this->validate(request(), [
+        'reg_no' => 'required',
+        'daily_rate' => 'required|numeric',
+        'image' => 'sometimes|image|mimes:jpeg,png,jpg,gif,svg|max:500',
+
+    ]);
+
+    // handles image uploading
+    if ($request->hasFile('image')){
+      // get the file name with the get_loaded_extensions
+      $fileNameWithExtension = $request->file('image')->getClientOriginalName();
+      // get the file name only
+      $fileName = pathinfo($fileNameWithExtension, PATHINFO_FILENAME);
+      // get the extenstion
+      $extension = $request->file('image')->getClientOriginalExtension();
+      // get the filename to store
+      $fileNameToStore = $fileName . '_' . time() . '.' . $extension;
+
+      // upload the image
+      $path = $request->file('image')->storeAs('public/images', $fileNameToStore);
+    } else {
+      $fileNameToStore = 'noImage.jpg';
+    }
+
+    // create a Vehicle object and set its values from the input
+    $vehicle = new Vehicle;
+    $vehicle->reg_no = $request->input('reg_no');
+    $vehicle->description = $request->input('description');
+    $vehicle->brand = $request->input('brand');
+    $vehicle->daily_rate = $rate->input('daily_rate');
+    $vehicle->created_at = now();
+    $vehicle -> image = $fileNameToStore;
+
+    // save the vehicle object
+    $vehicle->save();
+
+    // generate a redirectt HTTP response with a success message
+    return back()->with('success', 'Vehicle has been added');
+
+
+
     }
 
     /**
--- unit4/laracarent/app/Vehicle.php
+++ unit4/laracarent/app/Vehicle.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vehicle extends Model
 {
-    //
+    protected $fillable = ['reg_no', 'brand', 'description'];
 }
